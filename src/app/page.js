d6146ba09e26d5cf41b747a450babdf23b4acf8e
Bilal Haider <haiderbilal306@gmail.com>
2024-08-16 17:52:09 +0500

    added logo

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,18 @@
 "use client"
 import { useRouter } from 'next/navigation'
 import React from 'react'
+import Image from 'next/image'
+import logo from '../../public/assets/Bookmelogo.png'
 
-const page = () => {
+const Page = () => {
   const router =useRouter();
   return (
-    <div className='flex flex-col align-center justify-center h-[100vh] mx-auto w-[70%]'>
+    <div className='flex flex-col align-center justify-center mt-36 mx-auto w-[50%] rounded-md border p-10 bg-slate-100'>
+      <Image src={logo} width={200} height={200} />
       <h1 className='text-2xl my-5' >Restaurant Management System</h1>
       <button onClick={()=>router.push('/signin')} className='px-3 py-1 bg-blue-700 rounded-md text-white w-20'>Sign in</button>
     </div>
   )
 }
 
-export default page
+export default Page
