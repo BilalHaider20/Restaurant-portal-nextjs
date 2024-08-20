d7d4b5c4b1d471c57b841124c0a297feb89cc984
Bilal Haider <haiderbilal306@gmail.com>
2024-08-20 11:15:37 +0500

    added assets

--- public/assets/Images/index.js
+++ public/assets/Images/index.js
@@ -1,7 +1,11 @@
-const Images = {
-    menu_icon: require('public/Images/menu.svg'),
-    logo: require('public/Images/BookmeLogo.svg'),
+const images = {
+    menu_icon: require('./Menu.png'),
+    logo: require('./BookmeLogo.png'),
+    mosque_icon: require('./mdi_mosque.png'),
+    wallet_icon: require('./wallet.png'),
+    flight_icon: require('./flight-icon.png')
+    
 };
 
 
-export default Images;
+export default images;
