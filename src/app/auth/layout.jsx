b5391ba66ed483f54bcb864bdf147c921c19a8f1
hibah321 <hbhshahbaz321@gmail.com>
2024-08-20 14:11:58 +0500

    added sign up page

--- src/app/auth/layout.jsx
+++ src/app/auth/layout.jsx
@@ -1,11 +1,31 @@
 import Image from "next/image"
-export default function AuthLayout({children}){
-    return(
-        <html className="flex h-full">
-        <body className="flex">
-          {children}
-          <div className='flex flex-col items-center justify-center px-5 w-6/12 h-full'>
-            <div className="relative w-full h-4/6">
+import logo from '/public/assets/images/BookmeLogo.png'
+
+
+export default function AuthLayout({ children }) {
+  return (
+    <html className="flex h-full">
+
+      <body>
+        <div className="flex items-center justify-between px-6 p-3">
+          <Image src={logo} />
+          <a href="" className="text-primary-blue ">Help</a>
+        </div>
+        <div className="flex h-full">
+          <div className='flex flex-col items-center justify-center w-6/12 h-full bg-background-blue'>
+            <div className='container mx-auto w-9/12 text-center'>
+              <h1 className='text-3xl font-semibold'>Welcome to Bookme</h1>
+              <span className='text-lg font-extralight'>Please provide details below to login your account</span>
+            </div>
+            {children}
+            <p className='text-grey'>or continue with</p>
+            <div className='flex flex-col w-9/12 mt-3 space-y-2'>
+              <a className='btnSigninWith' href=""><Image src="/assets/images/google.png" width={15} height={15}></Image> Sign in with Google</a>
+              <a className='btnSigninWith' href=""><Image src="/assets/images/facebook.png" width={15} height={15}></Image> Sign in with Facebook</a>
+            </div>
+          </div>
+          <div className='flex flex-col items-center justify-center px-5 w-6/12 h-full border-box'>
+            <div className="relative w-full h-5/6">
               <Image
                 src="/assets/images/login-img.png"
                 alt="image"
@@ -14,7 +34,8 @@ export default function AuthLayout({children}){
               />
             </div>
           </div>
-        </body>
-      </html>
-    )
+        </div>
+      </body>
+    </html>
+  )
 }
--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -4,7 +4,7 @@ import { useRouter } from 'next/navigation'
 import "../../styles/globals.css"
 import Image from 'next/image'
 import { useForm } from 'react-hook-form'
-
+import { LuKeyRound } from "react-icons/lu";
 
 export default function Page() {
   const router = useRouter();
@@ -23,34 +23,28 @@ export default function Page() {
 
   return (
     
-      <div className='flex flex-col items-center justify-center w-6/12 h-full bg-background-blue'>
-        <div className='container mx-auto w-9/12 text-center'>
-          <h1 className='text-3xl font-semibold'>Welcome to Bookme</h1>
-          <span className='text-lg font-extralight'>Please provide details below to login your account</span>
-        </div>
+
         <form onSubmit={handleSubmit(onSubmit)} className='my-5 container mx-auto w-9/12 space-y-4'>
           <div className="space-y-2 w-full">
             <label className='m-0' htmlFor="phone">Phone Number</label>
             <input 
               {...register('phone')} 
               type="text" 
-              className="border-gray-400 m-0 py-3 px-4 block w-full rounded-lg text-sm border focus:border-0" 
+              className="inputForm" 
               placeholder="Phone Number" 
             />
           </div>
           <div className="relative space-y-2 w-full">
-            <label className='m-0' htmlFor="password">Password</label>
+            <label className='m-0' htmlFor="Password">Password</label>
             <input 
               {...register("password", { required: "Please enter your password" })} 
               type="password" 
               className="inputForm " 
               placeholder="Password" 
+              name='password'
             />
             <div className="formSvgDiv">
-              <svg className="w-5 h-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                <path d="M2 18v3c0 .6.4 1 1 1h4v-3h3v-3h2l1.4-1.4a6.5 6.5 0 1 0-4-4Z"></path>
-                <circle cx="16.5" cy="7.5" r=".5"></circle>
-              </svg>
+              <LuKeyRound className='text-slate-400' />
             </div>
             {errors.password && <p className="text-red-500" role="alert">{errors.password.message}</p>}
           </div>
@@ -71,15 +65,6 @@ export default function Page() {
 
           <button type="submit" className="bg-light-blue text-white py-2 px-4 rounded w-full">Sign In</button>
         </form>
-
-        <p className='text-grey'>or continue with</p>
-
-        <div className='flex flex-col w-9/12 mt-3 space-y-2'>
-          <a className='btnSigninWith' href=""><Image src="/assets/images/google.png" width={15} height={15}></Image> Sign in with Google</a>
-          <a className='btnSigninWith' href=""><Image src="/assets/images/facebook.png" width={15} height={15}></Image> Sign in with Facebook</a>
-        </div>
-
-      </div>
 
   );
 }
