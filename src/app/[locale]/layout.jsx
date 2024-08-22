863a345a2cede7aa3a884de455a4fdeb86c5405f
hibah321 <hbhshahbaz321@gmail.com>
2024-08-22 14:22:50 +0500

    fixed auth pages

--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -5,11 +5,7 @@ import StoreProvider from '../StoreProvider';
 import {NextIntlClientProvider} from 'next-intl';
 import PrelineScript from '../components/PrelineScript'
 import {getMessages} from 'next-intl/server';
-import Navbar from '../../components/Navbar'
-import logo from '/public/assets/images/BookmeLogo.png'
-import Footer from '../../components/Footer'
-import Sidebar from "../../components/Sidebar";
-import LayoutWrapper from "../../components/LayoutWrapper"
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,7 +20,7 @@ export default  async function RootLayout({ children ,locale }) {
     <html lang={locale}>
       <body className={` ${inter.className}`}>
         <NextIntlClientProvider messages={messages} locale={locale}>
-        <LayoutWrapper>{children}</LayoutWrapper>
+          {children}
         </NextIntlClientProvider>
       </body>
       <PrelineScript />
